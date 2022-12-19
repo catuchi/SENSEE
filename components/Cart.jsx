d8e0c3b26aeab70bec36aa4555ed99339a8a06ba
Chibu <atuchibueze@gmail.com>
2022-12-18 20:32:03 -0500

    implement stripe payment

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -11,9 +11,11 @@ import { toast } from "react-hot-toast";
 
 import { useGlobalStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
+import getStripe from "../lib/getStripe";
 
 const Cart = () => {
   const cartRef = useRef();
+
   const {
     totalPrice,
     totalQuantities,
@@ -22,6 +24,27 @@ const Cart = () => {
     toggleCartItemQuantity,
     onRemove,
   } = useGlobalStateContext();
+
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch("/api/stripe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if (response.statusCode === 500) return;
+
+    const data = await response.json();
+
+    toast.loading("Redirecting...");
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  };
+
   return (
     <div className="cart-wrapper" ref={cartRef}>
       <div className="cart-container">
@@ -105,7 +128,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button className="btn" type="button" onClick={""}>
+              <button className="btn" type="button" onClick={handleCheckout}>
                 Pay With Stripe
               </button>
             </div>
