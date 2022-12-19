765b642362a9013b77e6be319f8600d4c89a3e4f
Chibu <atuchibueze@gmail.com>
2022-12-19 07:21:48 -0500

    implement order success page

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
         submit_type: "pay",
         mode: "payment",
         payment_method_types: ["card"],
-        billing_address_collection: "auto",
+        billing_address_collection: "required",
         shipping_options: [
           { shipping_rate: "shr_1MGRaaIRfVlIGFmGfvt5VhB1" },
           { shipping_rate: "shr_1MGRbrIRfVlIGFmGnhwBfhHf" },
